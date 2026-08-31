44f33e0207518266400c91c518ff1d0f4603bdc7
mozstream01 <mozstream01@gmail.com>
2026-08-31 19:05:47 +0200

    adicionado a URL do back22

--- realtime-poll-full/frontend/src/lib/config.js
+++ realtime-poll-full/frontend/src/lib/config.js
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+export const API_URL = import.meta.env.VITE_API_URL || "https://real-time-poll-tjxj.onrender.com";
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || API_URL;
